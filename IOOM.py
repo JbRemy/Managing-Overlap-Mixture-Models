df2320542ed6de3ed45a46130dd57f1b83de5935
JbRemy <remyjeanb@gmail.com>
2018-01-26 19:49:54 +0100

    corrections

--- IOOM.py
+++ IOOM.py
@@ -16,17 +16,23 @@ class IOOM:
 
         return rate
 
-    def rate_ratio(self, x, z_1, z_2, Theta):
+    def rate_ratio(self, x, i, k, z_1, z_2, Theta):
 
         if self.Type == 'Binaries':
             rate = 1
-            for i in range(np.shape(x)[1]):
+            m = sum(z_1[:, k])-1
+            rate_m = (np.shape(x)[0]-m)/m
+            for k in range(np.shape(Theta)[0]):
                 for d in range(np.shape(Theta)[1]):
-                    theta_temp_1 = np.prod(np.power(Theta[:,d], z_1[i,:]))/(np.prod(np.power(1-Theta[:,d], z_1[i,:]))+ np.prod(np.power(Theta[:,d], z_1[i,:])))
-                    theta_temp_0 = np.prod(np.power(Theta[:,d], z_2[i,:]))/(np.prod(np.power(1-Theta[:,d], z_2[i,:]))+ np.prod(np.power(Theta[:,d], z_2[i,:])))
-                    rate_1 = (theta_temp_1**x[i,d])*((1-theta_temp_1)**(1-x[i,d]))
-                    rate_0 = (theta_temp_0 ** x[i,d]) * ((1 - theta_temp_0) ** (1 - x[i,d]))
-                    rate = rate*rate_1/rate_0
+                    rate = rate*np.exp(z_2[i, k]*x[i, d]*np.log(Theta[k, d]/(1+Theta[k, d])))/np.exp(z_1[i, k]*x[i, d]*np.log(Theta[k, d]/(1+Theta[k, d])))
+            rate = rate*rate_m
+            # for i in range(np.shape(x)[1]):
+            #     for d in range(np.shape(Theta)[1]):
+            #         theta_temp_1 = np.prod(np.power(Theta[:,d], z_1[i,:]))/(np.prod(np.power(1-Theta[:,d], z_1[i,:]))+ np.prod(np.power(Theta[:,d], z_1[i,:])))
+            #         theta_temp_0 = np.prod(np.power(Theta[:,d], z_2[i,:]))/(np.prod(np.power(1-Theta[:,d], z_2[i,:]))+ np.prod(np.power(Theta[:,d], z_2[i,:])))
+            #         rate_1 = (theta_temp_1**x[i,d])*((1-theta_temp_1)**(1-x[i,d]))
+            #         rate_0 = (theta_temp_0 ** x[i,d]) * ((1 - theta_temp_0) ** (1 - x[i,d]))
+            #         rate = rate*rate_0/rate_1
 
         return rate
 
@@ -59,6 +65,16 @@ class IOOM:
 
 
     def fit(self, x, K_init, Niter, alpha=1, omega=0.01, prop_new_clusts=True):
+        '''
+        Computes the estimated clusters and Theta
+        :param x: (np array) The unlabeled data
+        :param K_init: (int) Number of initial clusters
+        :param Niter: (int) Number of iterations
+        :param alpha: (float) parameter of the prior
+        :param omega: (float) parameter of the proposition
+        :param prop_new_clusts: (Boolean) allow new clusters
+        :return: (np array) clusters (np array) theta
+        '''
 
         n, d = np.shape(x)
         Theta = self.init_Theta(K_init, d)
@@ -68,14 +84,15 @@ class IOOM:
 
         for j in range(Niter):
             for i in range(n):
-                z_temp = z.copy()
-                z_temp[i,:] = 1
-                z_temp_2 = z.copy()
-                z_temp[i, :] = 0
                 for k in range(K):
+                    z_temp = z.copy()
+                    z_temp[i, k] = 1
+                    z_temp_2 = z.copy()
+                    z_temp[i, k] = 0
                     #p_1 = (sum(z_temp[:,k])-1)/n*self.rate(x[i,:], z_temp[i,:], Theta)
                     #p_0 = (n-sum(z_temp_2[:,k]))/n*self.rate(x[i,:], z_temp_2[i,:], Theta)
-                    p = self.rate_ratio(x, z_temp, z_temp_2, Theta)
+                    p = self.rate_ratio(x, i, k, z_temp, z_temp_2, Theta)
+                    print(1/(p+1))
                     z[i, k] = np.random.binomial(n=1, p=1/(p+1))
 
                 if prop_new_clusts:
